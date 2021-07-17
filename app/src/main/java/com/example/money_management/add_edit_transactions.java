54cd32851d1c8570860692ec853de63f234bcb7f
rafsalrahim <rafsalkrahim@gmail.com>
2021-07-17 18:43:16 +0530

    add edit transaction page added

---
+++ app/src/main/java/com/example/money_management/add_edit_transactions.java
@@ -0,0 +1,13 @@
+package com.example.money_management;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class add_edit_transactions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_edit_transactions);
+    }
+}
